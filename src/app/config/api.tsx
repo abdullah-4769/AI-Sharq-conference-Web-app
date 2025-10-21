b02db4274c0658e11083c046584a87c6b011c9d2
NasirSultan <rainasirsultan123@gmail.com>
2025-10-22 01:42:33 +0500

    s

--- src/app/config/api.tsx
+++ src/app/config/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://psychic-goldfish-p7prgwg6j5fxgx-3000.app.github.dev",
-  baseURL:  "http://localhost:5000",
+  baseURL:  "http://138.68.104.206:3000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
